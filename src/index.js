dafaa7f7e2c810d6be25e1cc9a3d7373d699b79b
cinyearchan <cinyearchan@gmail.com>
2021-09-16 13:59:14 +0800

    init syncHook project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-let oBtn = document.getElementById("btn")
-
-oBtn.addEventListener('click', function () {
-  import(/*webpackChunkName: 'login'*/'./login.js').then(login => {
-    console.log(login)
-  })
-})
-
-console.log('index.js 内容执行了')
