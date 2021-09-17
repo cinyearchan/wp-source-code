67004a2118ea0aa42025113c873e5b9d2e3f59fc
cinyearchan <cinyearchan@gmail.com>
2021-09-17 15:46:14 +0800

    source-code-map init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-let oBtn = document.getElementById("btn")
-
-oBtn.addEventListener('click', function () {
-  import(/*webpackChunkName: 'login'*/'./login.js').then(login => {
-    console.log(login)
-  })
-})
-
-console.log('index.js 内容执行了')
+console.log('index.js 内容执行')
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devtool: 'none',
   mode: 'development',
+  context: process.cwd(),
   entry: './src/index.js',
   output: {
-    filename: 'built.js',
+    filename: 'index.js',
     path: path.resolve('dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+  }
 }
