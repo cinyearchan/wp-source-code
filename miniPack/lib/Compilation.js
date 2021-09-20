2c2c2bd647cde5e9c8bef375479fe5e13c6d0ea1
cinyearchan <cinyearchan@gmail.com>
2021-09-20 17:41:38 +0800

    依赖模块处理:抽离创建空白模块代码

--- miniPack/lib/Compilation.js
+++ miniPack/lib/Compilation.js
@@ -38,16 +38,44 @@ class Compilation extends Tapable {
   }
 
   addModuleChain (context, entry, name, callback) {
-    // 创建一个模块，作为入口，用于加载其他所有模块
-    let entryModule = normalModuleFactory.create({
-      name,
-      context,
+    this.createModule({
+      name: name,
+      context: context,
       rawRequest: entry,
-      resource: path.posix.join(context, entry), // 找到 entry 入口的绝对路径
+      resource: path.posix.join(context, entry),
+      moduleId: './' + path.posix.relative(context, path.posix.join(context, entry)),
       parser
-    })
+    }, (entryModule) => {
+      this.entries.push(entryModule)
+    },callback)
+  }
 
-    const afterBuild = function (err, module) {
+  /**
+   * 创建空白模块，用于：加载入口模块；加载依赖模块
+   * @param {*} data 创建模块时需要的一些属性
+   * @param {*} doAddEntry 可选参数，在加载入口模块时，将入口模块的 id 写入 this.entries
+   * @param {*} callback 回调
+   */
+  createModule (data, doAddEntry, callback) {
+    /**
+     * 封装统一方法用于创建空白模块，该空白模块作为容器用于加载其他模块的内容
+     * 区分入口模块和依赖模块，通过 this.entries 和 this.modules 数组进行记录
+     * this.entries 记录入口模块
+     * this.modules 记录所有模块
+     */
+
+    // 创建一个模块，作为入口，用于加载其他所有模块
+    // let entryModule = normalModuleFactory.create({
+    //   name,
+    //   context,
+    //   rawRequest: entry,
+    //   resource: path.posix.join(context, entry), // 找到 entry 入口的绝对路径
+    //   parser
+    // })
+    // module 改名表示，创建的该空白模块，有可能用来加载入口模块，也可能用来加载被依赖模块
+    let module = normalModuleFactory.create(data)
+
+    const afterBuild = (err, module) => {
       // 在 afterBuild 中需要判断：当前次 module 加载完成之后是否需要处理依赖加载
       if (module.dependencies.length > 0) {
         // 当前模块 module 如果有依赖的模块，递归加载该依赖的模块
@@ -58,11 +86,13 @@ class Compilation extends Tapable {
       callback(err, module)
     }
 
-    this.buildModule(entryModule, afterBuild)
+    this.buildModule(module, afterBuild)
 
     // 当完成本次 build 操作后，保存 module
-    this.entries.push(entryModule)
-    this.modules.push(entryModule)
+    // this.entries.push(entryModule)
+    doAddEntry && doAddEntry(module)
+    // this.modules.push(entryModule)
+    this.modules.push(module)
   }
 
   /**
@@ -74,7 +104,7 @@ class Compilation extends Tapable {
     module.build(this, (err) => {
       // 当代码执行到此处，意味着当前 module 的编译完成
       this.hooks.succeedModule.call(module)
-      callback(err, mdoule)
+      callback(err, module)
     })
   }
 
@@ -87,7 +117,6 @@ class Compilation extends Tapable {
     // 加载模块的思路都是：创建一个模块，通过这个创建的空白模块加载被依赖的模块的内容
     // 重点：module 依赖的所有模块加载完成后，再执行回调 callback
     // neo-async
-    
   }
 }
 
